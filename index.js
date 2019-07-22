e4f084f1b8afa8f06fefc5d61bd3f509c83ff536
James Backhouse <jwbackhouse@gmail.com>
2019-07-22 15:25:43 +0100

    Added setup screen property and new model naming.

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const manualx = 400;
 const importFinishx = 950;
 const maximise = 1170;
 const nextx = 1200;
+const centre = 650;
+
+// x-positions (inclusive)
+// Input fields - property = 400 - 480
 
 // Set y variables for homescreen
 const buttons = 160
@@ -25,6 +29,11 @@ const seventh = 370;
 const eighth = 390;
 const ninth = 415;
 
+// Set y variables for new model popup
+const newName = 250;
+const newDesc = 280;
+const newAccept = 340;
+
 // Set y variables for model load screen
 const manualy = fourth;
 const importTraining = 420;
@@ -32,6 +41,12 @@ const importValidation = 560;
 const importFolder = 160;
 const importFinishy = 490;
 const nexty = 200;
+
+// Set y variables for model setup homescreen
+one = 320;
+two = 360;
+three = 380;
+four = 410;
 
 
 // Open DMway
@@ -44,14 +59,37 @@ const openDmway = () => {
   },12000)
 };
 
+// Select DMway when already open
+const selectDmway = () => {
+  robot.moveMouse(5,180);
+  robot.mouseClick('left');
+};
+
 // Open model
 const openModel = () => {
-  robot.moveMouse(projects,fifth);
+  robot.moveMouse(projects,second);
   robot.mouseClick('left');
   setTimeout(function() {
-    robot.moveMouse(models,first);
-    robot.mouseClick('left','double');
+    robot.moveMouse(models,buttons);
+    robot.mouseClick('left');
   },500)
+};
+
+// Name new model
+const nameNewModel = () => {
+  robot.moveMouse(centre,newName);
+  robot.mouseClick('left');
+  robot.typeString('NewModel1');
+  setTimeout(function() {
+    robot.moveMouse(centre,newDesc);
+    robot.mouseClick('left');
+    robot.typeString('Description');
+  },500);
+  setTimeout(function() {
+    robot.moveMouse(centre,newAccept);
+    robot.mouseClick('left');
+  },1000);
+
 };
 
 // Set upload to manual setting
@@ -140,19 +178,31 @@ const validationUpload = () => {
     robot.moveMouse(importFinishx,importFinishy);
     robot.mouseClick('left');
   },4000);
-};
 
-const next = () => {
   setTimeout(function() {
     robot.moveMouse(nextx,nexty);
     robot.mouseClick('left');
-  },1000);
+  },5000);
+};
+
+
+// Configure setup fields
+const setup = () => {
+  robot.moveMouse(400,one);
+  robot.mouseClick('left');
+  robot.keyTap('down');
+  robot.keyTap('down');
+  robot.keyTap('enter');
 };
 
 // Run upload functions and move to setup screen
-openDmway();
-setTimeout(openModel,13000);
-setTimeout(setManualUpload,17000);
-setTimeout(trainingUpload,17500);
-setTimeout(validationUpload,24000);
-setTimeout(next,28000);
+// openDmway();
+selectDmway();
+setTimeout(openModel,1000);
+setTimeout(nameNewModel,4000)
+// setTimeout(setManualUpload,17000);
+// setTimeout(trainingUpload,17500);
+// setTimeout(validationUpload,24000);
+// setTimeout(setup,28000);
+
+// Required for testing once DMway already launched
